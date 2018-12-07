23ed16df36b62100c62df277a2a44984e772d09d
Whitney Dilmurat <wjweini@gmail.com>
2018-12-07 22:04:18 +0800

    There'a a loop in InitPWM .

--- USER/SN76496.c
+++ USER/SN76496.c
@@ -9,11 +9,13 @@ void SN76496_Init(void)
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
   GPIO_InitStructure.GPIO_Pin = GPIO_Pin_All;			    
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 	
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;	
-  GPIO_Init(GPIOC, &GPIO_InitStructure);
-	GPIO_SetBits(GPIOC,GPIO_Pin_All);	 
-	CE_Write(Bit_RESET);
-
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;	
+  GPIO_Init(GPIOC, &GPIO_InitStructure); 
+	
+  SN76496_SendData(0x9f);
+  SN76496_SendData(0xbf);
+  SN76496_SendData(0xdf);
+  SN76496_SendData(0xff);
 }
  
 
@@ -21,11 +23,7 @@ uint8_t READY_Read(void)
 {
 	return GPIO_ReadInputDataBit(GPIOC,GPIO_Pin_8);
 }
- 
-void CE_Write( BitAction act)
-{
-		GPIO_WriteBit(GPIOC,GPIO_Pin_10,act);
-}
+  
   
 void WE_Write( BitAction act)
 { 
@@ -35,7 +33,7 @@ void WE_Write( BitAction act)
 
 void SN76496_SendData(unsigned char data)
 { 
-	  WE_Write(Bit_SET); 
+	WE_Write(Bit_SET); 
 	GPIO_WriteBit(GPIOC,GPIO_Pin_0,  (data&1)?Bit_SET:Bit_RESET);
 	GPIO_WriteBit(GPIOC,GPIO_Pin_1,  (data&2)?Bit_SET:Bit_RESET);
 	GPIO_WriteBit(GPIOC,GPIO_Pin_2,  (data&4)?Bit_SET:Bit_RESET);
@@ -44,8 +42,7 @@ void SN76496_SendData(unsigned char data)
 	GPIO_WriteBit(GPIOC,GPIO_Pin_5,  (data&32)?Bit_SET:Bit_RESET);
 	GPIO_WriteBit(GPIOC,GPIO_Pin_6,  (data&64)?Bit_SET:Bit_RESET);
 	GPIO_WriteBit(GPIOC,GPIO_Pin_7,  (data&128)?Bit_SET:Bit_RESET);
-	 
-	 
 	WE_Write(Bit_RESET);   
+	delay_ms(1);
 	WE_Write(Bit_SET);  
 }
--- USER/main.c
+++ USER/main.c
@@ -3,22 +3,45 @@
 #include "SN76496.h"
 #include "delay.h"
  
+ void mydelay(int d)
+ {
+	 int i = 0;
+	 for(i = 0; i < d;i++){}
+ }
  int main(void)
- {	 
+ {	  
+	  
+	 // sets channel 0 tone to 0x123
+	 
+	 	GPIO_InitTypeDef  GPIO_InitStructure;
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_All;			    
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 	
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;	
+  GPIO_Init(GPIOC, &GPIO_InitStructure);
+	 
+	 
 	 InitPWM();
 	 SN76496_Init();
 	 delay_init();
-	  
-	 // sets channel 0 tone to 0x123
-	 SN76496_SendData(0x83);
-	 SN76496_SendData(0x12);
+	 
+	 
+	// WE_Write(Bit_RESET);
+	 //SN76496_SendData(0x83);//‭10000011‬
+	 //SN76496_SendData(0x12);//‭00010010‬
+	 
 	 
 	 while(1)
-	 { 
-			SN76496_SendData(0x90); // sets channel 0 to loudest possible
-			delay_ms(100);   
-			SN76496_SendData(0x9F);  // sets channel 0 to lowest volume (silencing it)
-			delay_ms(100); 
+	 { 		
+		 // sets channel 0 to loudest possible 
+			//SN76496_SendData(0x90);//10010000‬  
+			//SN76496_SendData(0x9F);//10010000‬  
+				//b=!b;
+		 
+			/*
+			// sets channel 0 to lowest volume (silencing it)
+			SN76496_SendData(0x9F);   //‭10011111‬
+			delay_ms(100); */
 	 }
 	  
  }
