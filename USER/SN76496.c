8e8314372e42872d8518287ea61688005a7fc6ee
WD <wjweini@gmail.com>
2018-12-10 20:49:55 +0800

    COMPLETED. YEP.

--- USER/SN76496.c
+++ USER/SN76496.c
@@ -17,11 +17,15 @@ void SN76496_Init(void)
   GPIO_Init(GPIOC, &GPIO_InitStructure); 
 	WE_Write(Bit_SET);
 		    
-  //GPIO_InitStructure.GPIO_Mode = GPIO_Pin_8; 	  
-  //GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 	
- // GPIO_Init(GPIOC, &GPIO_InitStructure); 
+  GPIO_InitStructure.GPIO_Mode = GPIO_Pin_8; 	  
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 	
+  GPIO_Init(GPIOC, &GPIO_InitStructure); 
+	//mute all channels
 	
-
+	SN76496_SendData(0x9f);
+	SN76496_SendData(0xbf);
+	SN76496_SendData(0xdf);
+	SN76496_SendData(0xff);
 }
  
 
@@ -45,52 +49,41 @@ B7											B0
 */
 
 void SN76496_UpdateAttenuator(ESN76496Channel channel,unsigned char data)
-{
-	/*
+{ 
 	u8 _d = 0x90;//1001 0000
-	_d |= (channel<<5);
-	_d |= (data>>4);
-	SN76496_SendData(_d);*/
+	_d |= (channel& 0xf0);
+	_d |= (data & 0x0f);
+	SN76496_SendData(_d); 
 	
-	
-	u8 _d = 0xF0;//1111 0000
+	 
 	
 }
 
 void SN76496_SendData(unsigned char data)
 { 
 	// PULL DOWN WE
-	WE_Write(Bit_RESET); 
-	//WRITE BUS and keep data 
-	/*
-	u16 bb = data;
-	bb = bb << 8;*/
-	GPIOC->ODR = 0XFFFF; 
-	/*
-	data = ~data;
-    GPIOC->BSRR = (1U << 8) << (16 * ((data >> 0)&1));
-    GPIOC->BSRR = (1U << 9) << (16 * ((data >> 1)&1));
-    GPIOC->BSRR = (1U << 13) << (16 * ((data >> 2)&1));
-    GPIOC->BSRR = (1U << 14) << (16 * ((data >> 3)&1));
-    GPIOC->BSRR = (1U << 15) << (16 * ((data >> 4)&1));
-    GPIOC->BSRR = (1U << 0) << (16 * ((data >> 5)&1));
-    GPIOC->BSRR = (1U << 1) << (16 * ((data >> 6)&1));
-    GPIOC->BSRR = (1U << 2) << (16 * ((data >> 7)&1));*/
+	WE_Write(Bit_SET); 
+
 	
-	/*
-	GPIO_WriteBit(GPIOC,GPIO_Pin_0,  (data&1)?Bit_SET:Bit_RESET);
-	GPIO_WriteBit(GPIOC,GPIO_Pin_1,  (data&2)?Bit_SET:Bit_RESET);
-	GPIO_WriteBit(GPIOC,GPIO_Pin_2,  (data&4)?Bit_SET:Bit_RESET);
-	GPIO_WriteBit(GPIOC,GPIO_Pin_3,  (data&8)?Bit_SET:Bit_RESET);
-	GPIO_WriteBit(GPIOC,GPIO_Pin_4,  (data&16)?Bit_SET:Bit_RESET);
-	GPIO_WriteBit(GPIOC,GPIO_Pin_5,  (data&32)?Bit_SET:Bit_RESET);
-	GPIO_WriteBit(GPIOC,GPIO_Pin_6,  (data&64)?Bit_SET:Bit_RESET);
-	GPIO_WriteBit(GPIOC,GPIO_Pin_7,  (data&128)?Bit_SET:Bit_RESET);*/
-	//mydelay2(40000);
-	//wait for one PSG writecycle
-	//while(READY_Read()!=0){}//wait for ready change to low.  
-	//while(READY_Read()){}//wait for ready. 
+	GPIO_WriteBit(GPIOC,GPIO_Pin_0,  (data&128)?Bit_SET:Bit_RESET);
+	GPIO_WriteBit(GPIOC,GPIO_Pin_1,  (data&64)?Bit_SET:Bit_RESET);
+	GPIO_WriteBit(GPIOC,GPIO_Pin_2,  (data&32)?Bit_SET:Bit_RESET);
+	GPIO_WriteBit(GPIOC,GPIO_Pin_3,  (data&16)?Bit_SET:Bit_RESET);
+	GPIO_WriteBit(GPIOC,GPIO_Pin_4,  (data&8)?Bit_SET:Bit_RESET);
+	GPIO_WriteBit(GPIOC,GPIO_Pin_5,  (data&4)?Bit_SET:Bit_RESET);
+	GPIO_WriteBit(GPIOC,GPIO_Pin_6,  (data&2)?Bit_SET:Bit_RESET);
+	GPIO_WriteBit(GPIOC,GPIO_Pin_7,  (data&1)?Bit_SET:Bit_RESET);
+	
+	WE_Write(Bit_RESET); 
+//delay_us(14);
+  while(READY_Read()){}
+	while(READY_Read()!=0){}
+	//mydelay2(10);
+ 
 		
-	//PULL UP WE
-	WE_Write(Bit_SET);    
+	WE_Write(Bit_SET);   
+	//delay_ms(14);
+	//mydelay2(100);
+	
+	//PULL UP WE 
 }
--- USER/main.c
+++ USER/main.c
@@ -2,30 +2,141 @@
 #include "pwm.h"
 #include "SN76496.h"
 #include "delay.h"
- 
+#include "vgmdata.h"
+u16 waitSamples = 0;
+  u8 dataarray[4] =
+{
+	0x9f,
+	0xbf,
+	0xdf,
+	0xf0
+	
+	
+};
+int currnetVGMIndex = 0;
+u8 samplePlaying = 0;
+u8 readBuffer()
+{
+	 
+	u8 cmd = VGMDATA[currnetVGMIndex];
+	currnetVGMIndex++;
+	if(currnetVGMIndex >= VGNDATA_SIZE)
+	{
+		currnetVGMIndex = 0;
+	}
+	
+	return cmd;
+}
+
+
+ u16 readBuffer16()
+{
+  u16 d;
+  u8 v0 = readBuffer();
+  u8 v1 = readBuffer();
+  d = (v0 + (v1 << 8));
+  //bufferPos+=2;
+ // cmdPos+=2;
+  return d;
+}
+
+u16 proccessVGMCmd()
+{
+	u8 cmd = readBuffer();
+	
+	switch(cmd)
+  {
+    case 0x4F:
+		{
+    SN76496_SendData(0x06);
+    SN76496_SendData(readBuffer());
+		}
+    return 1;
+    case 0x50:
+		{
+    SN76496_SendData(readBuffer());
+		}
+    return 1;
+		    case 0x61:
+				{ 
+					return  readBuffer16();
+				}
+    case 0x62:
+    return 735;
+    case 0x63:
+    return 882;
+		case 0x7F:
+    {
+      return (cmd & 0x0F)+1;
+    }
+  }
+  return 1 ;
+	
+	
+	
+	
+	
+}
+
  void mydelay(int d)
  {
 	 int i = 0;
 	 for(i = 0; i < d;i++){}
  }
+ 
+void Tick(void)
+{
+ 
+  if(waitSamples > 0)
+	{
+    waitSamples--;
+		return;
+	}
+	if(waitSamples == 0 && 0==samplePlaying)
+  {
+    samplePlaying = 1;
+    waitSamples += proccessVGMCmd();
+    samplePlaying = 0;
+    return;
+  } 
+}
+ 
  int main(void)
  {	  
-	  
-	 // sets channel 0 tone to 0x123
-	 
-	 u8 blab = 0; 
-	  
+	   u8 blab = 0;
+	 u8 fourMTick = 160;
+	 u8 curFTick = fourMTick;
 	 InitPWM();
 	 SN76496_Init(); 
-	   
-	 
+	 //delay_init();
+ 
 	
+	//SN76496_SendData(0x9f);
+	//SN76496_SendData(0xbf);
+	//SN76496_SendData(0xdf);
+	//SN76496_SendData(0xff);
+	 
 	 while(1)
 	 { 		
-	 
-		 		SN76496_SendData(blab++);//10010000‬  
-				mydelay(300000);
+		if(curFTick>0)
+		{
+			curFTick--;
+		}
+		else
+		{
+			curFTick = fourMTick;
+		 Tick();
+		}
+		
+		 //mydelay(100);
+		// delay_ms(proccessVGMCmd());
+		 
+		 //delay_ms(20);
+		// 		SN76496_SendData(blab++);//10010000‬  
+ 
+	//		SN76496_UpdateAttenuator	
 		
 	 }
 	  
  }
+ 
